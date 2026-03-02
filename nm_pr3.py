96346c30dc1c54e3bed6ea8e9aa613dbf42e692e
RobertL2809 <robert.lee2809@gmail.com>
2026-03-02 23:19:16 +0200

    parandasin paar viga
    Added print statement for the number of iterations.

--- nm_pr3.py
+++ nm_pr3.py
@@ -1,7 +1,7 @@
 import math
 
 def f(x):
-    return -2 * x * math.exp(-x) + math.exp(-2 * x) + x**2
+    return -2 * x * math.exp(-x) + math.exp(-2 * x) + x**2 
 
 def df(x):
     return -2 * math.exp(-x) + 2 * x * math.exp(-x) - 2 * math.exp(-2 * x) + 2 * x
@@ -11,17 +11,18 @@ def newton(x0, f, df, eps, maxit):
     x_varasem= x0
     for i in range(maxit):
         x_uus = x_varasem - f(x_varasem) / df(x_varasem)
-        if abs(x_uus - x_varasem) <= eps:
+        if abs(x_uus - x_varasem) < eps:
             return x_uus, i+1
         x_varasem = x_uus
 
     return x_uus, maxit
 
 x0 = 0.0
-eps = 1e-5
+eps = 1e-5 #epsiloni väärtus
 maxit = 100
-
+#Väljastan lahendi ja iteratsioonide arvu
 lahend, sammud = newton(x0, f, df, eps, maxit)
 
 print(lahend)
+
 print(sammud)  
